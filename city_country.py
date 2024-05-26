8c7584bccd36e3eb99f16d8b8fb1dd58872cc8d4
Mark-Jayson <markjaysonagao10@gmail.com>
2024-05-27 07:29:53 +0800

    Finished index.html

--- city_country.py
+++
@@ -1,8 +0,0 @@
-def city_country(city, country):
-    City = city.title()
-    Country = country.title()
-    return City + ", " + Country
-
-print(city_country("Pasig Ciy", "Philippines"))
-print(city_country("Bangkok", "Thailand"))
-print(city_country("Taipei", "Taiwan"))
